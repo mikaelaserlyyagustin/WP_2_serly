4993bac7a310673c50b8b715183c7a2ae6af7698
mikaelaserlyagustin <https://github.com/mikaelaserlyyagustin/WP_2_serly.git>
2022-10-12 12:35:17 +0700

    tugas ke 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya mikaela serly agustin
+ Saya tinggal di daerah putusibau
+ olah raga yang saya sukai adalah
+bola voli dan tenis meja";
+ }
+}
