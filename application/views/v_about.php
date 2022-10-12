4cbee13cad0237de70e2f8f9f04344f85fe50778
mikaelaserlyagustin <https://github.com/mikaelaserlyyagustin/WP_2_serly.git>
2022-10-12 16:09:42 +0700

    tugas ke 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : mikaela serly</li>
+ <li>Nama Belakang : Agustin</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li>jl. patinggi sari pala pulau</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>putussibau</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>bola voli</li>
+ <li>tenis meja</li>
+ </ul>
+</section>
